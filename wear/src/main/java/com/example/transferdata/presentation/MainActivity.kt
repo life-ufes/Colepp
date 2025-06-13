029e982e1136f9f4bddc1dab965949527a1cea68
Vinicius <vinijaguare@gmail.com>
2025-06-12 22:21:39 -0300

    add more sensors and pong response

--- wear/src/main/java/com/example/transferdata/presentation/MainActivity.kt
+++ wear/src/main/java/com/example/transferdata/presentation/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import com.example.commons.Capabilities.Companion.ACCELEROMETER_CAPABILITY
+import com.example.commons.Capabilities.Companion.AMBIENT_TEMPERATURE_CAPABILITY
+import com.example.commons.Capabilities.Companion.GYROSCOPE_CAPABILITY
+import com.example.commons.Capabilities.Companion.HEART_RATE_CAPABILITY
 import com.google.android.gms.wearable.Wearable
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +20,9 @@ class MainActivity : ComponentActivity() {
     private val capabilityClient by lazy { Wearable.getCapabilityClient(this) }
 
     private var accelerometerSensor: Sensor? = null
+    private var ambientTemperatureSensor: Sensor? = null
+    private var heartRateSensor: Sensor? = null
+    private var gyroscopeSensor: Sensor? = null
     private lateinit var sensorManager: SensorManager
 
     private val mainViewModel by viewModels<MainViewModel>()
@@ -24,8 +30,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        ambientTemperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
 
         setContent {
             WearApp(mainViewModel)
@@ -35,41 +45,55 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun configureObservers() {
-        mainViewModel.accelerometerState.observe(this) { isActive ->
+        mainViewModel.sensorsListenerState.observe(this) { isActive ->
             if (isActive) {
-                registerAccelerometerListener()
+                registerSensorsListener()
             } else {
-                unregisterAccelerometerListener()
+                unregisterSensorListener()
             }
         }
     }
 
-    private fun registerAccelerometerListener() {
+    private fun registerSensorsListener() {
         accelerometerSensor?.let {
-            sensorManager.registerListener(
-                mainViewModel,
-                it,
-                DELAY_FOR_ACCELEROMETER_25HZ
-            )
+            sensorManager.registerListener(mainViewModel, it, DELAY_FOR_ACCELEROMETER_25HZ)
+        }
+        ambientTemperatureSensor?.let {
+            sensorManager.registerListener(mainViewModel, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        heartRateSensor?.let {
+            sensorManager.registerListener(mainViewModel, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        gyroscopeSensor?.let {
+            sensorManager.registerListener(mainViewModel, it, DELAY_FOR_GYROSCOPE_50HZ)
         }
     }
 
-    private fun unregisterAccelerometerListener() {
+    private fun unregisterSensorListener() {
         sensorManager.unregisterListener(mainViewModel)
     }
 
     private fun checkSensorAvailability() {
-        if (accelerometerSensor == null) {
-            Log.d(TAG, "Accelerometer sensor not available")
-        } else {
-            capabilityClient.addLocalCapability(ACCELEROMETER_CAPABILITY)
-                .addOnFailureListener { exception ->
-                    exception.printStackTrace()
-                }
-                .addOnSuccessListener {
-                    Log.d(TAG, "Local capability $ACCELEROMETER_CAPABILITY added successfully")
-                    Log.d(TAG, "Accelerometer sensor available")
-                }
+        val sensorMap = mapOf(
+            ACCELEROMETER_CAPABILITY to accelerometerSensor,
+            AMBIENT_TEMPERATURE_CAPABILITY to ambientTemperatureSensor,
+            HEART_RATE_CAPABILITY to heartRateSensor,
+            GYROSCOPE_CAPABILITY to gyroscopeSensor
+        )
+
+        sensorMap.forEach { (capability, sensor) ->
+            if (sensor == null) {
+                Log.d(TAG, "$capability sensor not available")
+            } else {
+                capabilityClient.addLocalCapability(capability)
+                    .addOnFailureListener { exception ->
+                        exception.printStackTrace()
+                    }
+                    .addOnSuccessListener {
+                        Log.d(TAG, "Local capability $capability added successfully")
+                        Log.d(TAG, "Accelerometer sensor available")
+                    }
+            }
         }
     }
 
@@ -82,19 +106,28 @@ class MainActivity : ComponentActivity() {
     override fun onPause() {
         super.onPause()
         messageClient.removeListener(mainViewModel)
-        unregisterAccelerometerListener()
+        unregisterSensorListener()
 
-        capabilityClient.removeLocalCapability(ACCELEROMETER_CAPABILITY)
-            .addOnFailureListener { exception ->
-                exception.printStackTrace()
-            }
-            .addOnSuccessListener {
-                Log.d(TAG, "Local capability $ACCELEROMETER_CAPABILITY removed successfully")
-            }
+        val capabilityList = listOf(
+            ACCELEROMETER_CAPABILITY,
+            AMBIENT_TEMPERATURE_CAPABILITY,
+            HEART_RATE_CAPABILITY,
+            GYROSCOPE_CAPABILITY
+        )
+        capabilityList.forEach { capability ->
+            capabilityClient.removeLocalCapability(capability)
+                .addOnFailureListener { exception ->
+                    exception.printStackTrace()
+                }
+                .addOnSuccessListener {
+                    Log.d(TAG, "Local capability $capability removed successfully")
+                }
+        }
     }
 
     companion object {
         private const val TAG = "MainActivity"
-        private const val DELAY_FOR_ACCELEROMETER_25HZ = 40_000 // 25Hz
+        private const val DELAY_FOR_ACCELEROMETER_25HZ = 40_000
+        private const val DELAY_FOR_GYROSCOPE_50HZ = 20_000
     }
 }
--- wear/src/main/java/com/example/transferdata/presentation/MainViewModel.kt
+++ wear/src/main/java/com/example/transferdata/presentation/MainViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.os.SystemClock
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -13,14 +15,24 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.commons.AccelerometerData
+import com.example.commons.AmbientTemperatureData
 import com.example.commons.CommunicationPaths.Companion.ACCELEROMETER_DATA_PATH
-import com.example.commons.CommunicationPaths.Companion.INIT_ACCELEROMETER_TRANSFER_DATA_PATH
-import com.example.commons.CommunicationPaths.Companion.STOP_ACCELEROMETER_TRANSFER_DATA_PATH
+import com.example.commons.CommunicationPaths.Companion.AMBIENT_TEMPERATURE_DATA_PATH
+import com.example.commons.CommunicationPaths.Companion.GYROSCOPE_DATA_PATH
+import com.example.commons.CommunicationPaths.Companion.HEART_RATE_DATA_PATH
+import com.example.commons.CommunicationPaths.Companion.INIT_TRANSFER_DATA_PATH
+import com.example.commons.CommunicationPaths.Companion.PING_PATH
+import com.example.commons.CommunicationPaths.Companion.PONG_PATH
+import com.example.commons.CommunicationPaths.Companion.STOP_TRANSFER_DATA_PATH
+import com.example.commons.GyroscopeData
+import com.example.commons.HeartRateData
 import com.google.android.gms.wearable.MessageClient
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.Wearable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainViewModel(
     application: Application
@@ -31,44 +43,93 @@ class MainViewModel(
     private val messageClient = Wearable.getMessageClient(application)
     private val nodeClient = Wearable.getNodeClient(application)
 
-    private val _accelerometerState = MutableLiveData<Boolean>()
-    val accelerometerState: LiveData<Boolean> = _accelerometerState
+    private val _sensorsListenerState = MutableLiveData<Boolean>()
+    val sensorsListenerState: LiveData<Boolean> = _sensorsListenerState
 
     private var currentNodeId: String? = null
 
     override fun onMessageReceived(messageEvent: MessageEvent) {
         when (messageEvent.path) {
-            INIT_ACCELEROMETER_TRANSFER_DATA_PATH -> {
-                startAccelerometerTransfer(messageEvent.sourceNodeId)
+            PING_PATH -> {
+            val t2 = SystemClock.elapsedRealtimeNanos()
+            val data = ByteBuffer.allocate(Long.SIZE_BYTES)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .putLong(t2)
+                .array()
+
+            sendPongMessage(messageEvent.sourceNodeId, messageEvent.data + data)
+        }
+
+            INIT_TRANSFER_DATA_PATH -> {
+                startSensorsListener(messageEvent.sourceNodeId)
             }
 
-            STOP_ACCELEROMETER_TRANSFER_DATA_PATH -> {
-                stopAccelerometerTransfer(messageEvent.sourceNodeId)
+            STOP_TRANSFER_DATA_PATH -> {
+                stopSensorListeners(messageEvent.sourceNodeId)
             }
         }
     }
 
-    private fun startAccelerometerTransfer(nodeId: String) {
-        currentNodeId = nodeId
-        _accelerometerState.postValue(true)
+    private fun sendPongMessage(sourceNodeId: String, bytes: ByteArray) {
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("DataLayerListenerService", "Sending PONG message to node: $sourceNodeId")
+            messageClient.sendMessage(
+                sourceNodeId,
+                PONG_PATH,
+                bytes
+            )
+        }
     }
 
-    private fun stopAccelerometerTransfer(nodeId: String) {
+    private fun startSensorsListener(nodeId: String) {
+        currentNodeId = nodeId
+        _sensorsListenerState.postValue(true)
+    }
+
+    private fun stopSensorListeners(nodeId: String) {
         if (nodeId == currentNodeId) {
             currentNodeId = null
-            _accelerometerState.postValue(false)
+            _sensorsListenerState.postValue(false)
         }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-            val data = AccelerometerData(
-                x = event.values[0],
-                y = event.values[1],
-                z = event.values[2],
-                timestamp = event.timestamp
-            )
-            sendAccelerometerData(data)
+        when (event?.sensor?.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+                val data = AccelerometerData(
+                    x = event.values[0],
+                    y = event.values[1],
+                    z = event.values[2],
+                    timestamp = event.timestamp,
+                )
+                sendAccelerometerData(data)
+            }
+
+            Sensor.TYPE_AMBIENT_TEMPERATURE -> {
+                val data = GyroscopeData(
+                    x = event.values[0],
+                    y = event.values[1],
+                    z = event.values[2],
+                    timestamp = event.timestamp,
+                )
+                sendGyroscopeData(data)
+            }
+
+            Sensor.TYPE_HEART_RATE -> {
+                val data = HeartRateData(
+                    heartRate = event.values[0].toInt(),
+                    timestamp = event.timestamp,
+                )
+                sendHeartRateData(data)
+            }
+
+            Sensor.TYPE_GYROSCOPE -> {
+                val data = AmbientTemperatureData(
+                    temperature = event.values[0],
+                    timestamp = event.timestamp,
+                )
+                sendAmbientTemperatureData(data)
+            }
         }
     }
 
@@ -82,15 +143,50 @@ class MainViewModel(
                             node.id,
                             ACCELEROMETER_DATA_PATH,
                             dataBytes
-                        ).addOnSuccessListener {
-                            // Handle success
-                        }.addOnFailureListener {
-                            // Handle failure
-                        }
+                        )
                     }
                 }.addOnFailureListener {
                     // Handle failure to get connected nodes
                 }
+        }
+    }
+
+    private fun sendAmbientTemperatureData(data: AmbientTemperatureData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val dataBytes = data.toByteArray()
+            currentNodeId?.let { nodeId ->
+                messageClient.sendMessage(
+                    nodeId,
+                    AMBIENT_TEMPERATURE_DATA_PATH,
+                    dataBytes
+                )
+            }
+        }
+    }
+
+    private fun sendHeartRateData(data: HeartRateData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val dataBytes = data.toByteArray()
+            currentNodeId?.let { nodeId ->
+                messageClient.sendMessage(
+                    nodeId,
+                    HEART_RATE_DATA_PATH,
+                    dataBytes
+                )
+            }
+        }
+    }
+
+    private fun sendGyroscopeData(data: GyroscopeData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val dataBytes = data.toByteArray()
+            currentNodeId?.let { nodeId ->
+                messageClient.sendMessage(
+                    nodeId,
+                    GYROSCOPE_DATA_PATH,
+                    dataBytes
+                )
+            }
         }
     }
 
