7111f8c2125e21d390c6b5e07bc331df720a4f62
Vinicius <vinijaguare@gmail.com>
2025-07-13 23:01:19 -0300

    remove unused capabilities client

--- wear/src/main/java/com/example/transferdata/presentation/SampleApplication.kt
+++ wear/src/main/java/com/example/transferdata/presentation/SampleApplication.kt
@@ -1,9 +1,6 @@
 package com.example.transferdata.presentation
 
 import android.app.Application
-import com.google.android.gms.wearable.Wearable
 
 class SampleApplication : Application() {
-
-    val capabilityClient by lazy { Wearable.getCapabilityClient(this) }
 }
