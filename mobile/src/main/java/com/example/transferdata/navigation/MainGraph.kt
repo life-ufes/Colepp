9aa700ec3c9f49dc06e5daae356d67f5a458c89c
Vinicius <vinijaguare@gmail.com>
2025-07-13 22:42:12 -0300

    create edit record screen

--- mobile/src/main/java/com/example/transferdata/navigation/MainGraph.kt
+++ mobile/src/main/java/com/example/transferdata/navigation/MainGraph.kt
@@ -1,5 +1,6 @@
 package com.example.transferdata.navigation
 
+import android.net.Uri
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -9,14 +10,18 @@ import com.example.transferdata.navigation.MainNavGraphArgs.RECORD_ID
 import com.example.transferdata.navigation.MainNavGraphRoutes.HOME_GRAPH
 import com.example.transferdata.navigation.MainNavGraphRoutes.HOME_SCREEN
 import com.example.transferdata.navigation.MainNavGraphRoutes.RECORD_DETAIL_SCREEN
+import com.example.transferdata.navigation.MainNavGraphRoutes.RECORD_EDIT_SCREEN
 import com.example.transferdata.navigation.NewRecordingNavGraphRoutes.CREATE_NEW_RECORDING_GRAPH
 import com.example.transferdata.presentation.home.HomeScreen
 import com.example.transferdata.presentation.recordDetail.RecordDetailScreen
+import com.example.transferdata.presentation.recordEdit.RecordEditScreen
+import java.io.File
 
 object MainNavGraphRoutes {
     const val HOME_GRAPH = "home_graph"
     const val HOME_SCREEN = "home_screen"
     const val RECORD_DETAIL_SCREEN = "record_detail_screen"
+    const val RECORD_EDIT_SCREEN = "record_edit_screen"
 }
 
 object MainNavGraphArgs {
@@ -25,9 +30,9 @@ object MainNavGraphArgs {
 
 internal fun NavGraphBuilder.addMainGraph(
     navController: NavHostController,
-    createDatasetFile: (Long) -> Unit,
+    createDatasetFile: (Long, (File) -> Unit) -> Unit,
     onBackPressed: () -> Unit,
-    onClosePressed: () -> Unit
+    shareFile: (Uri) -> Unit
 ) {
     navigation(
         route = HOME_GRAPH,
@@ -35,7 +40,6 @@ internal fun NavGraphBuilder.addMainGraph(
     ) {
         composable(HOME_SCREEN) {
             HomeScreen(
-                onRecordingPressed = {},
                 createNewRecording = {
                     navController.navigate(
                         CREATE_NEW_RECORDING_GRAPH
@@ -50,12 +54,31 @@ internal fun NavGraphBuilder.addMainGraph(
                         }"
                     )
                 },
+                onEditClicked = { recordId ->
+                    navController.navigate(
+                        "$RECORD_EDIT_SCREEN/$RECORD_ID=${
+                            JsonHandler.getEncodedJsonParamAsUri(recordId)
+                        }"
+                    )
+                }
             )
         }
         composable("$RECORD_DETAIL_SCREEN/${RECORD_ID}={${RECORD_ID}}") {
             RecordDetailScreen(
                 onBackPressed = onBackPressed,
                 createDatasetFile = createDatasetFile,
+                shareFile = shareFile,
+            )
+        }
+        composable("$RECORD_EDIT_SCREEN/${RECORD_ID}={${RECORD_ID}}") {
+            RecordEditScreen(
+                onBackPressed = onBackPressed,
+                onSave = {
+                    navController.popBackStack(
+                        route = HOME_SCREEN,
+                        inclusive = false
+                    )
+                }
             )
         }
     }
--- mobile/src/main/java/com/example/transferdata/navigation/MainNavHost.kt
+++ mobile/src/main/java/com/example/transferdata/navigation/MainNavHost.kt
@@ -1,20 +1,22 @@
 package com.example.transferdata.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.transferdata.MainViewModel
 import com.example.transferdata.navigation.MainNavGraphRoutes.HOME_GRAPH
+import java.io.File
 
 @Composable
 internal fun MainNavHost(
     navController: NavHostController = rememberNavController(),
     mainViewModel: MainViewModel,
     onBackPressed: () -> Unit,
-    onClosePressed: () -> Unit,
     setKeepScreenFlag: (Boolean) -> Unit,
-    createDatasetFile: (Long) -> Unit,
+    createDatasetFile: (Long, (File) -> Unit) -> Unit,
+    shareFile: (Uri) -> Unit
 ) {
     NavHost(
         navController = navController,
@@ -23,14 +25,13 @@ internal fun MainNavHost(
         addMainGraph(
             navController = navController,
             onBackPressed = onBackPressed,
-            onClosePressed = onClosePressed,
-            createDatasetFile = createDatasetFile
+            createDatasetFile = createDatasetFile,
+            shareFile = shareFile
         )
         addNewRecordingGraph(
             navController = navController,
             mainViewModel = mainViewModel,
             onBackPressed = onBackPressed,
-            onClosePressed = onClosePressed,
             setKeepScreenFlag = setKeepScreenFlag
         )
     }
