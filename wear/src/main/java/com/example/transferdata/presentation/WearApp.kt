facf429bd2ad9d699d07ae829a16598ac2b39e11
Vinicius <vinijaguare@gmail.com>
2025-08-05 01:36:55 -0300

    remove unused import directive

--- wear/src/main/java/com/example/transferdata/presentation/WearApp.kt
+++ wear/src/main/java/com/example/transferdata/presentation/WearApp.kt
@@ -1,7 +1,6 @@
 package com.example.transferdata.presentation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
